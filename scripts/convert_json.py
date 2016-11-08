98f3301f2e1e29977b0236f921eea35715ddf26f
jowa <joachim.suico@gmail.com>
2016-11-08 11:11:00 +0800

    added task_id script and updated paths
    added script folder for modularity of future scripts. just add option in
    argparse. also fixed some constant paths.

--- scripts/convert_json.py
+++ scripts/convert_json.py
@@ -4,6 +4,7 @@
 convert_json.py
 
 script called by cuckoo2mist.py. Processes task ids
+arguments, start and end task id number, are given in main script
 """
 
 __author__ = "joa suico"
@@ -11,22 +12,26 @@ __version__ = "0.1"
 
 import os, sys
 import glob
+import fnmatch
 
-ALL = "all_analyses"
+def process_tasks(ALL_ANALYSES, run_folder, start, end):
+    from cuckoo2mist import class_mist
 
-def process_tasks():
-    run_folder = raw_input("CBAQ run folder: ")
-    start = int(raw_input("start task id: "))
-    end = int(raw_input("end task id: "))
+    json_files = []
+    for root, dirname, filenames in os.walk(os.path.join(ALL_ANALYSES, run_folder)):
+        for filename in fnmatch.filter(filenames, "report.json"):
+            json_files.append(os.path.join(root, filename))
 
-    myfolder = os.path.join(ALL, run_folder)
-
-    if os.path.exists(myfolder):
-        from cuckoo2mist import class_mist
-    else:
-        print "Folder does not exist.\n"
-        print sys.exit()
-
-
-if __name__ == '__main__':
-    process_tasks()
+    task_ids = range(start, end+1)
+    needed_files = [path for path in json_files for i in task_ids if "/{}/".format(i) in path]
+    
+    for i in needed_files:
+        try:
+            cm = class_mist.mistit(i)
+            if cm.parse() and cm.convert():
+                temp_path = i.replace("report.json", "report.mist")
+                cm.write(os.path.join("reports", temp_path.replace("/", "-")))
+        except Exception as e:
+            print "Cannot convert {}".format(i)
+            print e
+            continue
